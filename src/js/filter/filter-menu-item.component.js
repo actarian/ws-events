63e97978421c08ab76b6dfaa6d8c3d0e59f68366
Luca Zampetti <lzampetti@gmail.com>
2020-06-01 18:23:09 +0200

    ws-events

--- src/js/filter/filter-menu-item.component.js
+++ src/js/filter/filter-menu-item.component.js
@@ -25,7 +25,7 @@ FilterMenuItemComponent.meta = {
 		<span class="option" [class]="{ active: filter.has(item), disabled: item.disabled, selected: item.selected }">
 			<span class="checkbox" (click)="filter.toggle(item)"></span>
 			<span class="name" [class]="{ menu: filter.isMenuItem(item), active: item.active }" (click)="toggleActive(item)" [innerHTML]="item.label"></span>
-			<span class="count" [innerHTML]="item.count || ''"></span>
+			<span class="count" [innerHTML]="item.count"></span>
 		</span>
 		<ul class="nav--options" *if="filter.isMenuItem(item) && (item.active)">
 			<li class="nav--options__menu" menu-item [filter]="item" [item]="option" *for="let option of item.options"></li>

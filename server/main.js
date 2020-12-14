6990e497a7575d29c4fb2b63e1a6b8984c28ccd7
Luca Zampetti <lzampetti@gmail.com>
2020-12-14 14:50:17 +0100

    w-academy

---
+++ server/main.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const HOST = `http://localhost:${PORT}`;
+
+var app = express();
+app.disable('x-powered-by');
+app.use(express.static(path.join(__dirname, '../docs/')));
+app.use('/ws-events', serveStatic(path.join(__dirname, '../docs/')));
+app.use('/Modules/Events/Client/docs/', serveStatic(path.join(__dirname, '../docs/')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname, '../docs/index.html'));
+});
+app.listen(PORT, () => {
+	console.log(`NodeJs Running server at ${HOST}`);
+});
